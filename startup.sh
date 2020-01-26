faeb71a60779f64f03c18bfacfc1a891a469ad5b
frank-bailey <59938273+frank-bailey@users.noreply.github.com>
2020-01-26 13:08:05 -0500

    Added extra info to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+# bash script to startup the drone.py script
+#
+# updated: 2020-Jan-26
+# author: Frank Bailey
+#
+
 
 if [ "$(ps -eaf | grep -e [d]rone.py | awk '{print$2}')" == "" ]; then
         sudo python /usr/share/drone/wifidrone/drone.py
